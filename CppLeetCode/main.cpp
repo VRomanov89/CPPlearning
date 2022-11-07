bcd41a6281775080a0d81ee59fda1ae214fc4701
Vladimir Romanov <vladromweb@gmail.com>
2022-11-07 08:37:17 -0500

    Problem 13 Solution
    Problem 13 Solution

--- CppLeetCode/main.cpp
+++ CppLeetCode/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
-int main() {
-    int n;
-    cin >> n;
-    cout << "test";
+
+// create a class
+class Solutions
+{
+
+public:
+    int romanToInt(string s); //13. Roman to Integer
+};
+
+int Solutions::romanToInt(string s) //13. Roman to Integer
+{
+    unordered_map<char, int> mp{
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000},
+    };
+    int ans = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (mp[s[i]] < mp[s[i + 1]])
+            ans -= mp[s[i]];
+        else
+            ans += mp[s[i]];
+    }
+    return ans;
+}
+
+int main()
+{
+    Solutions sol;
+    cout <<sol.romanToInt("XXX");
 }
