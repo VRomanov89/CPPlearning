76edfcc483e11b3fe96dee542687ecd6df09facb
Vladimir Romanov <vladromweb@gmail.com>
2022-11-13 18:51:21 -0500

    Class getters / setters
    Class getters / setters

--- CppLeetCode/Cylinder.h
+++ CppLeetCode/Cylinder.h
@@ -23,4 +23,24 @@ public:
     {
         return PI * base_radius * base_radius * height;
     }
+
+    // Functions - Getters
+    double get_base_radius()
+    {
+        return base_radius;
+    }
+    double get_height()
+    {
+        return height;
+    }
+
+    // Functions - Setters
+    void set_base_radius(double base)
+    {
+        base_radius = base;
+    }
+    void set_height(double hgt)
+    {
+        height = hgt;
+    }
 };
