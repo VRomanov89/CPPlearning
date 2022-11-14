a9fe69383ea4779281181c1acd9dc606f83d1682
Vladimir Romanov <vladromweb@gmail.com>
2022-11-13 21:28:32 -0500

    Summation Triangle #1
    Summation Triangle #1

--- CPP_CodeWars/kata.cpp
+++ CPP_CodeWars/kata.cpp
@@ -11,3 +11,18 @@ std::string even_or_odd(int number)
         return "Odd";
     }
 }
+
+unsigned long long get_sum(unsigned int n) // Summation Triangle #1 https://www.codewars.com/kata/6357825a00fba284e0189798/train/cpp
+{
+    unsigned long long sum = 0;
+    // your code here
+    for (unsigned int i = 0; i <= n; i++)
+    {
+        for (unsigned int j = 0; j <= i; j++)
+        {
+            sum = sum + 2 * j + i + 1;
+        }
+    }
+
+    return sum;
+}
